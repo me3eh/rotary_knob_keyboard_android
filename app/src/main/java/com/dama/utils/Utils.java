c2b4c1d75b7bf286a1c078ff9064022c8c1310d8
DMar00 <xmad.management@gmail.com>
2023-05-31 08:02:44 +0200

    Add animation cursor

---
+++ app/src/main/java/com/dama/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.dama.utils;
+
+public class Utils {
+    public static String colorToString(int color){
+
+        return null;
+    }
+}
--- app/src/main/java/com/dama/views/CursorSpaceView.java
+++ app/src/main/java/com/dama/views/CursorSpaceView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,10 +30,11 @@ public class CursorSpaceView extends FrameLayout {
         //init cursor
         Drawable cs = getResources().getDrawable(R.drawable.cursor);
         cursor = new KeyView(getContext(), cs, null, "#FBFBFB");
-        moveCursor(keyView);
+        //moveCursor(keyView);
+        initPosition(keyView);
     }
 
-    public void moveCursor(KeyView keyView){
+    private void initPosition(KeyView keyView){
         keyView.post(() -> {
             Rect offsetViewBounds = new Rect();
             keyView.getDrawingRect(offsetViewBounds);
@@ -52,6 +55,51 @@ public class CursorSpaceView extends FrameLayout {
 
             cursor.changeDimension(keyView.getKeyHeight(), keyView.getKeyWidth(), 0);
         });
+    }
+
+    public void moveCursor(KeyView keyView){
+        //calculate destination coordinates
+        Rect offsetViewBounds = new Rect();
+        keyView.getDrawingRect(offsetViewBounds);
+        offsetDescendantRectToMyCoords(keyView, offsetViewBounds);
+        int x = offsetViewBounds.left;
+        int y = offsetViewBounds.top;
+
+        //create animation
+        TranslateAnimation animation = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0f, Animation.ABSOLUTE, x - cursor.getX(),
+                Animation.RELATIVE_TO_SELF, 0f, Animation.ABSOLUTE, y - cursor.getY()
+        );
+        animation.setDuration(350);
+
+        //add listener
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //final view's position at the animation's end
+                cursor.clearAnimation();
+                LayoutParams layoutParams = new LayoutParams(
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                );
+                layoutParams.leftMargin = x;
+                layoutParams.topMargin = y;
+                cursor.setLayoutParams(layoutParams);
+
+                cursor.changeDimension(keyView.getKeyHeight(), keyView.getKeyWidth(), 0);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        //start animation on view
+        cursor.startAnimation(animation);
     }
 
     public void destroyCursor(){
